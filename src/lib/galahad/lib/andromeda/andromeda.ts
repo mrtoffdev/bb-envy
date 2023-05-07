50e265adf45b4d3e4f54de1ff1e15ee226045f40
mrtoffdev <pub.mrtoff@proton.me>
2023-05-07 15:34:46 +0800

    Moved scheduler logic from galahad.ts to andromeda.ts

--- src/lib/galahad/lib/andromeda/andromeda.ts
+++ src/lib/galahad/lib/andromeda/andromeda.ts
@@ -11,46 +11,78 @@ import { _SRC } from '../axioms/sourcefiles'
 
 export async function main (ns: NS){
 
+    const InitSchedulerState: SchedulerState = {
+        ns         : ns,
+        active     : true,
+
+        start_time : new Date().getTime(),
+        cycles     : 0,
+        clock      : 0,
+        speed      : 20,
+        offset     : 0,
+        RT         : 0,
+        TT         : 0,
+    }
 }
 
 export class Scheduler {
-    ns          : NS;
-    active      : boolean;
+    
+    state       : SchedulerState;
 
-    clock       : number;
-    speed       : number;
-    cycles      : number;
-    start_time  : number;
-    TT          : number;
-    RT          : number;
-    offset      : number;
-
-    constructor (ns: NS, clock_speed: number){
-
-        this.ns         = ns;
-        this.active     = true;
-
-        this.start_time = new Date().getTime();
-        this.cycles     = 0;
-        this.clock      = 0;
-        this.speed      = clock_speed;
-        this.offset     = 0
-        this.RT         = 0;
-        this.TT         = 0;
+    constructor (ns: NS, state: SchedulerState){
+        this.state      = state;
     }
 
     async handler (callback ?: (logs ?: string) => {}){
-        while (this.active) {
+        while (this.state.active) {
             if (callback != undefined) callback();
 
             // handler events
 
 
-            this.TT = this.cycles * this.speed;
-            this.RT = new Date().getTime() - this.start_time;
-            this.offset = this.RT - this.TT;
 
-            await this.ns.sleep(this.speed - this.offset);
+            // refresh clocks & enforce correction
+
+            this.state.TT       = this.state.cycles * this.state.speed;
+            this.state.RT       = new Date().getTime() - this.state.start_time;
+            this.state.offset   = this.state.RT - this.state.TT;
+
+            await this.state.ns.sleep(this.state.speed - this.state.offset);
         }
+    }
+
+    Deploy(ns: NS, OPERATION: number, target: string, threads: lock_n_arr4){
+        const UUID      = crypto.randomUUID();
+        const operation = Strategy_L;
+        let op_code     = 0;
+
+        let cfg: AxiomCFG = {
+            target  : target,
+            mode    : 'once',
+            delay   : 0,
+            id      : UUID,
+        }
+        switch (OPERATION) {
+            case 0:
+                ns.exec(_SRC.spoof, 'home', threads[0], target, 0, UUID, 0);
+                op_code = 0;
+                break;
+            case 1:
+                ns.exec(_SRC.exploit, 'home', threads[1], target, 0, UUID, 0);
+                op_code = 1;
+                break;
+            case 2:
+                ns.exec(_SRC.deposit, 'home', threads[2], target, 0, UUID, 0);
+                op_code = 2;
+                break;
+            case 3:
+                ns.exec(_SRC.exploit, 'home', threads[3], target, 0, UUID, 0);
+                op_code = 1;
+                break;
+            default:
+                ns.exit();
+        }
+
+        log (ns, 'scriptlog', `[Andromeda] : Deployed ${operation[op_code]}`)
     }
 }
